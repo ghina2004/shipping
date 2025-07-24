4d2622e355a8d5a7c541246a804c207ffd8c7337
ZeinaAlbarre <107673684+ZeinaAlbarre@users.noreply.github.com>
2025-07-24 17:18:45 +0300

    Update MessageSent.php

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -6,12 +6,12 @@ use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class MessageSent implements ShouldBroadcast
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -26,7 +26,7 @@ class MessageSent implements ShouldBroadcast
 
     public function broadcastWith(): array
     {
-        Log::info('real_real');
+        Log::info('real_realll');
 
         return [
             'id' => $this->message->id,
